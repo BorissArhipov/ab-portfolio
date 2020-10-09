81ad60017101643784854a12af27a3dd7bfc1b94
Boriss Arhipov <bori.7166455@gmail.com>
2020-10-09 22:19:57 +0300

    Setting up an enviroment

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                hello world
+            </div>
+        );
+    }
+}
+
+export default App;
